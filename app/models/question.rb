320a491b6ce5e42096a596b85e6b4e5222ace9c7
Anton Koh <anton.koh@gmail.com>
2016-01-24 16:58:28 +0000

    Answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
+  has_many :answers, dependent: :destroy
+
   validates :body, presence: true
   validates :title, presence: true
   validates :title, length: {maximum: 200}
